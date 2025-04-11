6d46dec3463af08b1dfa47d544e76f1584975abe
BiniyamGetachew1001 <biniyam.getachew@aastustudent.edu.et>
2025-04-11 15:01:30 +0300

    feat: restore navigation links in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,13 @@ const Navbar = () => {
   return (
     <nav>
       <ul className="flex space-x-4">
-        {/* Navigation links removed */}
+        <li><Link to="/home" className="hover:text-gray-300">Home</Link></li>
+        <li><Link to="/book-summaries" className="hover:text-gray-300">Book Summaries</Link></li>
+        <li><Link to="/book-purchase" className="hover:text-gray-300"><span className="icon-class">📚</span> Book Purchase</Link></li>
+        <li><Link to="/business-plans" className="hover:text-gray-300">Business Plans</Link></li>
+        <li><Link to="/blog" className="hover:text-gray-300">Blog</Link></li>
+        <li><Link to="/bookmarks" className="hover:text-gray-300">Bookmarks</Link></li>
+        <li><Link to="/offline-library" className="hover:text-gray-300">Offline Library</Link></li>
       </ul>
     </nav>
   );
